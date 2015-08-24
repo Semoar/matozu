c67ccbf0172b7bab4951ffd59ae243877768a25a
Simon Dreher <code@simon-dreher.de>
2015-08-24 21:08:19 +0200

    Initial commit with basic features
    You can add markers either by clicking on the map or by searching for a
    place. All markers are stored in a JSON object which is displayed and can be
    copied manually into index.htm to share the markers with others.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Share markers
+
+This project makes it easy to place and name markers on a map.
+
+Soon coming will be the feature to generate a simple share URL to send it to other
+people. For now you have to host these files somewhere (just static files) and
+copy a JSON object into the index.htm file.
+
+
+# Used software & license
+
+* Leaflet.js
+* bootstrap
+
+This software is licensed under MIT-License
---
+++ share-markers.js
@@ -0,0 +1,87 @@
+// Temporary marker when hovering a search result.
+var tempMarker;
+var redIcon = L.icon({
+    iconUrl: 'images/marker-icon-red.png',
+    iconRetinaUrl: 'images/marker-icon-red-2x.png',
+    iconSize:    [25, 41],
+    iconAnchor:  [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'images/marker-shadow.png',
+    shadowSize:  [41, 41]
+})
+function showTempMarker(lat, lng) {
+    tempMarker = new L.marker([lat, lng], {icon: redIcon});
+    markerLayers.addLayer(tempMarker);
+}
+function removeTempMarker() {
+    markerLayers.removeLayer(tempMarker);
+    tempMarker = null;
+}
+
+function searchPlace() {
+    var query = document.getElementById('search-text').value;
+    var url = 'https://nominatim.openstreetmap.org/search?q=' + query + '&format=json';
+    var searchResultsHTML = document.getElementById('search-results');
+    // clear previous search results
+    searchResultsHTML.innerHTML = '';
+    $.getJSON(url, function(data){
+        for (var i=0; i<data.length; i++) {
+            var place = data[i];
+            var li = document.createElement('li');
+            li.className = 'list-group-item';
+            li.setAttribute('data-lat', place['lat']);
+            li.setAttribute('data-lng', place['lon']);
+            li.appendChild(document.createTextNode(place['display_name']));
+            li.addEventListener("click", function() {
+                addMarker(this.innerHTML, this.getAttribute('data-lat'), this.getAttribute('data-lng'));
+            });
+            li.onmouseover = function() {
+                showTempMarker(this.getAttribute('data-lat'), this.getAttribute('data-lng'))
+            };
+            li.onmouseout = removeTempMarker;
+            searchResultsHTML.appendChild(li);
+        }
+    });
+    // Say form to not really submit the search form to the server.
+    return false;
+}
+
+function drawMarkers() {
+    markerLayers.clearLayers();
+    for (var title in markers) {
+        marker = new L.marker(markers[title], {draggable:'true', title: title});
+        marker.on('dragend', function(event) {
+            m = event.target;
+            markers[m.options.title] = [m.getLatLng()['lat'], m.getLatLng()['lng']];
+            document.getElementById('json-content').value = JSON.stringify(markers, null, ' ');
+        });
+        marker.bindPopup('<a href="./edit.htm?id=' + title + '">' + title + '</a>').openPopup();
+        markerLayers.addLayer(marker);
+    }
+    document.getElementById('json-content').innerHTML = JSON.stringify(markers, null, ' ');
+}
+
+function addMarker(title, lat, lon) {
+    var title = prompt('Title of the note:', title);
+    if (title == null) {
+        return;
+    }
+    if (title == '') {
+        title = uuid();
+    }
+    // ToDo: or if title already used
+    markers[title] = [lat, lon];
+    drawMarkers();
+    // ToDo: Somehow save to server
+    // 1. Get user content out
+    // 2. Load to server
+    // (3. Generate share URL)
+}
+
+// Helper
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
