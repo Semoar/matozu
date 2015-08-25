0d522c83ebb6bb925d4a4d9c16130e82237861c0
Simon Dreher <code@simon-dreher.de>
2015-08-25 14:41:26 +0200

    Set of markers selectable by URL parameter
    By adding the parameter 'id' to the url you can specify a filename in
    'data' to load. Example: '.../index.htm?id=london' loads the file
    'data/london' and shows the markers defined in this file. For testing
    this commit adds two example files ('london' and 'paris').

--- share-markers.js
+++ share-markers.js
@@ -85,3 +85,9 @@ function uuid() {
         return v.toString(16);
     });
 }
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
